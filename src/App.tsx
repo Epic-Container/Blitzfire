a9654615e0306f7c09609682df91bb6a661e1cf7
Epic-Container <smpdrive0@gmail.com>
2024-06-11 10:31:59 +0700

    recomit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
-const App:React.FC = () => {
+import React, { useState } from 'react'
+
+const App: React.FC = () => {
+  let [count, setCount] = useState(0)
+  function clickMe() {
+    setCount(count + 1)
+  }
   return (
-    <div>
-      <Button>Testing</Button>
-    </div> 
+    <div className="flex items-center justify-center h-screen">
+        <div className='flex flex-col gap-4'>  
+          <h1>There is nothing we can do</h1>
+          <button onClick={clickMe} className='bg-blue-900 rounded-lg text-white p-2'>Click Me: {count}</button>
+        </div>
+    </div>
   )
 }
 export default App;
